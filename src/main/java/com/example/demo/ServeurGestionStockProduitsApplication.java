45ca134a8e7d9644feb4e4abb3267ec80c59be9d
mtahiana <mahatr9@yahoo.fr>
2020-03-25 10:28:28 +0400

    authentication

--- src/main/java/com/example/demo/ServeurGestionStockProduitsApplication.java
+++ src/main/java/com/example/demo/ServeurGestionStockProduitsApplication.java
@@ -13,11 +13,11 @@ public class ServeurGestionStockProduitsApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
 		
-		ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
+		/*ProduitRepository produitRepository = ctx.getBean(ProduitRepository.class);
 		
 		produitRepository.save(new Produit("Livre", 50, 20));
 		produitRepository.save(new Produit("Cahier", 200, 5.25f));
-		produitRepository.save(new Produit("Stylo", 500, 2.10f));
+		produitRepository.save(new Produit("Stylo", 500, 2.10f));*/
 	}
 
 }
