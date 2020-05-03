67f177235670ab0cf61687ba03efc909a0ff108e
mtahiana <mahatr9@yahoo.fr>
2020-05-03 14:12:50 +0400

    refactor3

---
+++ src/main/java/com/example/demo/config/initData/InitProducts.java
@@ -0,0 +1,30 @@
+package com.example.demo.config.initData;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.entity.Produit;
+import com.example.demo.repository.ProduitRepository;
+
+@Component
+@Order(3)
+public class InitProducts implements ApplicationRunner {
+	
+	private final ProduitRepository produitRepository;
+	
+	public InitProducts(ProduitRepository produitRepository) {
+		this.produitRepository = produitRepository;
+	}
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		/*System.out.println("Init products .....");
+		
+		produitRepository.save(new Produit("Livre", 50, 20));
+		produitRepository.save(new Produit("Cahier", 200, 5.25f));
+		produitRepository.save(new Produit("Stylo", 500, 2.10f));*/
+	}
+
+}
---
+++ src/main/java/com/example/demo/config/initData/InitUsers.java
@@ -0,0 +1,47 @@
+package com.example.demo.config.initData;
+
+import java.util.Arrays;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.util.RoleEnum;
+
+@Component
+@Order(2)
+public class InitUsers implements ApplicationRunner {
+	
+	private final UserRepository userRepository;
+	private final RoleRepository roleRepository;
+	
+	
+	public InitUsers(UserRepository userRepository, RoleRepository roleRepository) {
+		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
+	}
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		/*System.out.println("Init users .....");
+		
+		Role roleUser = roleRepository.findByName(RoleEnum.ROLE_USER.getName());
+		Role roleAdmin = roleRepository.findByName(RoleEnum.ROLE_ADMIN.getName());
+		
+		User user = new User("user", "password1", true);
+		user.setRoles(Arrays.asList(roleUser));
+		
+		userRepository.save(user);
+		
+		User admin = new User("admin", "password2", true);
+		admin.setRoles(Arrays.asList(roleUser, roleAdmin));
+		
+		userRepository.save(admin);*/
+	}
+
+}
